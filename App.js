315735492cbd15f2a76b63c12a2d35466535a62b
Muhammad Haris <muhammadharis4999@gmail.com>
2022-01-27 16:24:37 +0500

    push notification is Done

--- App.js
+++ App.js
@@ -11,21 +11,14 @@ import InAppNotification from './src/components/InAppNotification/view';
 AppRegistry.registerHeadlessTask(
   'RNFirebaseBackgroundMessage',
   messaging().setBackgroundMessageHandler(async remoteMessage => {
-    const data = remoteMessage.data;
-
-    if (data.type === 'seekRequest') {
-      setTimeout(() => {
-        store.dispatch(setCallData(data));
-        store.dispatch(setCallModal(true));
-      }, 2000);
-    }
+    console.log('Background notifications:    ', remoteMessage);
   }),
 );
 
 const App = () => {
   useEffect(() => {
     getToken();
-    checkNotification();
+    //checkNotification();
   }, []);
 
   const getToken = () => {
@@ -37,31 +30,31 @@ const App = () => {
       });
   };
 
-  const checkNotification = () => {
-    addNotificationListener = () => {
-      this.messageForeGround();
-      this.messageBackGround();
-      messaging().onNotificationOpenedApp(remoteMessage => {
-        console.log('onNotificationOpenedApp', remoteMessage.notification);
-      });
-      messaging()
-        .getInitialNotification()
-        .then(remoteMessage => {
-          console.log('getInitialNotification', remoteMessage);
-        });
-    };
-    messageForeGround = () => {
-      return messaging().onMessage(async remoteMessage => {
-        EventRegister.emit('InAppNotification', remoteMessage);
-        console.log('onMessageForGround====>', remoteMessage);
-      });
-    };
-    messageBackGround = () => {
-      messaging().setBackgroundMessageHandler(async remoteMessage => {
-        // console.log('setBackgroundMessageHandler', remoteMessage);
-      });
-    };
-  };
+  // const checkNotification = () => {
+  //   addNotificationListener = () => {
+  //     this.messageForeGround();
+  //     this.messageBackGround();
+  //     messaging().onNotificationOpenedApp(remoteMessage => {
+  //       console.log('onNotificationOpenedApp', remoteMessage.notification);
+  //     });
+  //     messaging()
+  //       .getInitialNotification()
+  //       .then(remoteMessage => {
+  //         console.log('getInitialNotification', remoteMessage);
+  //       });
+  //   };
+  //   messageForeGround = () => {
+  //     return messaging().onMessage(async remoteMessage => {
+  //       EventRegister.emit('InAppNotification', remoteMessage);
+  //       console.log('onMessageForGround====>', remoteMessage);
+  //     });
+  //   };
+  //   messageBackGround = () => {
+  //     messaging().setBackgroundMessageHandler(async remoteMessage => {
+  //       // console.log('setBackgroundMessageHandler', remoteMessage);
+  //     });
+  //   };
+  // };
 
   const requestUserPermission = async () => {
     await messaging().requestPermission();
@@ -75,18 +68,18 @@ const App = () => {
     .getInitialNotification()
     .then(remoteMessage => {
       if (remoteMessage) {
-        console.log('MESSAGE:   ', remoteMessage);
+        console.log('initialNotifition MESSAGE:   ', remoteMessage);
       }
     })
     .catch(reason => console.log('App::getInitialNotification', reason));
 
   messaging().onNotificationOpenedApp(remoteMessage => {
-    console.log('MESSAGE:   ', remoteMessage);
+    console.log('onNotification open MESSAGE:   ', remoteMessage);
   });
 
   useEffect(() => {
     const unsubscribe = messaging().onMessage(remoteMessage => {
-      console.log('MESSAGE:   ', remoteMessage);
+      console.log('unSubcribe  MESSAGE:   ', remoteMessage);
     });
 
     return () => {
@@ -97,13 +90,6 @@ const App = () => {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <StackNavigation />
-        <InAppNotification
-          vibrate
-          interval={4500}
-          onPress={remoteMessage => {
-            // console.log('remoteMessage', remoteMessage);
-          }}
-        />
       </PersistGate>
     </Provider>
   );
